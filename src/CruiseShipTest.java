2d85587bd1a2ebbbdbb172f861baeeabbbd307ef
DoroDore <tlyz82@student.cis.eduhk>
2024-12-03 13:29:02 +0800

    Minor detail changes

--- src/CruiseShipTest.java
+++ src/CruiseShipTest.java
@@ -14,6 +14,10 @@ class CruiseShipTest {
     private WalkingActivity walkingActivity1, walkingActivity2;
     private TheaterActivity theaterActivity1;
     private Passenger passenger1, passenger2, passenger3, passenger4;
+
+    /**
+     * Set up the test fixture before each test.
+     */
     @BeforeEach
     void setUp() {
         destination1 = new Destination("Destination 1");
@@ -39,6 +43,10 @@ class CruiseShipTest {
                 new ArrayList<>(List.of(destination1, destination2)));
 
     }
+
+    /**
+     * Tear down the test fixture after each test.
+     */
     @AfterEach
     void tearDown() {
         cruiseShip1 = null;
@@ -48,6 +56,11 @@ class CruiseShipTest {
         walkingActivity2 = null;
         theaterActivity1 = null;
     }
+
+    /**
+     * Test the constructor and verifies whether passengers are assigned seat numbers accordingly.
+     * Also verifies that once added, the amount of passengers
+     */
     @Test
     void addPassengersTest() {
         cruiseShip1.addPassenger(passenger1);
@@ -58,31 +71,36 @@ class CruiseShipTest {
         assertEquals(2, passenger2.getSeatNumber());
         assertEquals(3, passenger3.getSeatNumber());
     }
+
+    /**
+     * Double checks whether the exact same passenger can be added to the ship twice
+     * If it does, then the array list's size will be wrong.
+     */
     @Test
     void addPassengerTwiceTest() {
         cruiseShip1.addPassenger(passenger1);
         cruiseShip1.addPassenger(passenger1);
         assertEquals(1, cruiseShip1.getPassengerList().size());
     }
+
+    /**
+     * Tests whether a passengers can join the activity. Additionally, it checks the passenger's
+     * balance to assure it has been altered correctly.
+     */
     @Test
     void joinActivity() {
-        destination1.addActivity(walkingActivity1);
         destination1.addActivity(theaterActivity1);
-        cruiseShip1.addPassenger(passenger1);
-        cruiseShip1.addPassenger(passenger2);
-        cruiseShip1.addPassenger(passenger3);
-        cruiseShip1.joinActivity(passenger1, walkingActivity1, destination1);
-        cruiseShip1.joinActivity(passenger2, walkingActivity1, destination1);
-        cruiseShip1.joinActivity(passenger3, walkingActivity1, destination1);
-        assertEquals(3, walkingActivity1.getParticipants().size());
-        assertEquals(47, walkingActivity1.getActivityCapacity());
         cruiseShip1.joinActivity(passenger1, theaterActivity1, destination1);
         cruiseShip1.joinActivity(passenger2, theaterActivity1, destination1);
         cruiseShip1.joinActivity(passenger3, theaterActivity1, destination1);
         assertEquals(3, theaterActivity1.getParticipants().size());
         assertEquals(97, theaterActivity1.getActivityCapacity());
-        assertEquals(975 ,passenger3.getBalance());
+        assertEquals(980 ,passenger3.getBalance());
     }
+
+    /**
+     * Verifies whether the description of the activity can be correctly accessed.
+     */
     @Test
     void verifyActivityDescription() {
         destination1.addActivity(walkingActivity1);
@@ -99,6 +117,11 @@ class CruiseShipTest {
         assertEquals("Join tour guide Neil Armstrong to the moon!", walkingActivity1.getActivityDescription());
         assertEquals("A tale of achoos", theaterActivity1.getActivityDescription());
     }
+
+    /**
+     * Verifies whether the passenger can be removed from the ship.
+     * Additionally, it checks if everyone's seat values shift up by one.
+     */
     @Test
     void removePassengerTest() {
         cruiseShip1.addPassenger(passenger1);
@@ -110,6 +133,11 @@ class CruiseShipTest {
         assertEquals(1, passenger2.getSeatNumber());
         assertEquals(2, passenger3.getSeatNumber());
     }
+
+    /**
+     * Verifies whether a passenger can be removed from the ship when they are not on the ship.
+     * If they are not on the ship, then the array list's size will be wrong.
+     */
     @Test
     void removePassengerNotOnShipTest() {
         cruiseShip1.addPassenger(passenger1);
@@ -117,6 +145,11 @@ class CruiseShipTest {
         cruiseShip1.removePassenger(passenger3);
         assertEquals(2, cruiseShip1.getPassengerList().size());
     }
+
+    /**
+     * Verifies whether a person will be able to join an activity if their balance is insufficient
+     * If they can, then the participants size will be incorrect.
+     */
     @Test
     void noMoneyForActivity() {
         destination1.addActivity(walkingActivity1);
@@ -126,11 +159,30 @@ class CruiseShipTest {
         assertEquals(50, walkingActivity1.getActivityCapacity());
         assertEquals(0, walkingActivity1.getParticipants().size());
     }
+
+    /**
+     * Double checks whether a passenger can
+     */
     @Test
     void seniorityDiscount() {
         destination1.addActivity(walkingActivity1);
         cruiseShip1.addPassenger(passenger4);
         cruiseShip1.joinActivity(passenger4, walkingActivity1, destination1);
         assertEquals(995.5, passenger4.getBalance());
+    }
+
+    /**
+     * Tests whether a participant can join two activities from the same destination.
+     * Since this shouldn't be allowed, the second activity should have no participants
+     */
+    @Test
+    void joinTwoActivitiesInSameDestination() {
+        destination1.addActivity(walkingActivity1);
+        destination1.addActivity(theaterActivity1);
+        cruiseShip1.addPassenger(passenger1);
+        cruiseShip1.joinActivity(passenger1, walkingActivity1, destination1);
+        cruiseShip1.joinActivity(passenger1, theaterActivity1, destination1);
+        assertEquals(1, walkingActivity1.getParticipants().size());
+        assertEquals(0, theaterActivity1.getParticipants().size());
     }
 }
